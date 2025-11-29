b59f3265c0236a00c4491e85ee42b828e449758c
Xiaoyue Lyu <xlyu00green@gmail.com>
2025-11-29 16:45:37 +0800

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "fmt" and "os" imports in stage 1 (feel free to remove this!)
@@ -12,4 +14,19 @@ var _ = os.Stdout
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
 	fmt.Fprint(os.Stdout, "$ ")
+	readUserCommand()
+}
+
+func readUserCommand() {
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	check(err)
+
+	command = strings.TrimSpace(command)
+	fmt.Printf("%s: command not found", command)
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
